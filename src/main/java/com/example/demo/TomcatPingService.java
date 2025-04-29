a690d3131d85fa5436bc921d00467e9cb6539c8c
QuartiQus <62442320+QuartiQus@users.noreply.github.com>
2025-04-29 10:36:40 +0200

    Changed tomcatPingService

--- src/main/java/com/example/demo/TomcatPingService.java
+++ src/main/java/com/example/demo/TomcatPingService.java
@@ -6,12 +6,12 @@ import org.springframework.web.client.RestTemplate;
 public class TomcatPingService {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String tomcatUrl = "http://localhost:8080/tomcat";
 
 
     public boolean isTomcatUp() {
         try {
-            ResponseEntity<String> response = restTemplate.getForEntity();
+            String tomcatUrl = "http://localhost:8080/tomcat";
+            ResponseEntity<String> response = restTemplate.getForEntity(tomcatUrl, String.class);
         } catch (Exception e) {
             System.out.println("Tomcat is not up" + e.getMessage());
             return false;
