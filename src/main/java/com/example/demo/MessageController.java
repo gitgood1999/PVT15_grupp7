6f1618064b455de5a648f705ceecc9a9155d90ea
gitgood1999 <gustav.dahlfors99@gmail.com>
2025-05-03 17:48:52 +0200

    Lade till MessageService och MessageController
    Man kan hitta alla meddelanden baserat på chatID och returnera som lista

---
+++ src/main/java/com/example/demo/MessageController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/chat/{chatId}")
+    public List<Message> getMessagesByChat(@PathVariable Long chatId) {
+        return messageService.getMessagesByChatId(chatId);
+    }
+}
+
---
+++ src/main/java/com/example/demo/MessageService.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getMessagesByChatId(long chatId){
+        return messageRepository.findByChatId(chatId);
+    }
+}
