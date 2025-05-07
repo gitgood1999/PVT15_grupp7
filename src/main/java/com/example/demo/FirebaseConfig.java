1de38d8778a9da521cd50b6fa534f78e14cb44ac
QuartiQus <62442320+QuartiQus@users.noreply.github.com>
2025-05-07 11:16:29 +0200

    added a firebaseClass

---
+++ src/main/java/com/example/demo/FirebaseConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/firebase-service-account.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
